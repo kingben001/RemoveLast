6e41e121a3609cf034573a152d74585aa41a9351
kingben001 <kingben001@yahoo.com>
2022-10-07 19:56:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        myLinkedList.reverse();
+
+
+
+        myLinkedList.printList();
+    }
+}
